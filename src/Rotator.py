898c6dafc2d4ab66f05c51ec5fbc6ffd58ebc8dc
AG6GR <slhe@princeton.edu>
2015-01-22 16:40:06 -0500

    Implemented setPosition and printPosition methods

--- src/Rotator.py
+++ src/Rotator.py
@@ -10,6 +10,7 @@ class Rotator :
 		
 	def getAzimuth(self) :
 		"Returns the current Azimuth of the antenna"
+		rotor.flushInput();
 		# Request Azimuth mode
 		self.rotor.write("A\r")
 		# Read response, example response: "A\r\nA=150.5 S=6 S\r\n"
@@ -29,9 +30,27 @@ class Rotator :
 		startIndex = response.find("E=") + 2
 		endIndex = startIndex + response[startIndex:].find(" ")
 		return float(response[startIndex:endIndex])
-
+		
+	def setPosition(self, reqAzimuth, reqElevation) :
+		"Commands rotor toward requested azimuth and elevation"
+		# Ensure requested position is within limits
+		if reqAzimuth < 1: reqAzimuth = 1
+		if reqAzimuth > 359: reqAzimuth = 359
+		if reqElevation < 0: reqElevation = 0
+		if reqElevation > 80: reqElevation = 80
+		
+		# Issue Azimuth command
+		self.rotor.flushInput();
+		self.rotor.write("A" + str(reqAzimuth) + "\r")
+		# Issue Elevation command
+		self.rotor.write("E" + str(reqElevation) + "\r")
+		return
+		
+	def printPosition(self)
+		print("Azimuth: " + self.getAzimuth() + " Elevation: " + self.getElevation())
+		return
 # Test code
 import serial
 
 rotor = Rotator("COM1")
-print rotor.getAzimuth()
+print rotor.printPosition()
