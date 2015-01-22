6ff8eef494466959e7be8e30ac9e75ca2d9e4c4a
AG6GR <slhe@princeton.edu>
2015-01-22 17:16:52 -0500

    Implemented stop(), fixed setPosition() timing, added documentation

--- src/Rotator.py
+++ src/Rotator.py
@@ -1,5 +1,5 @@
 class Rotator :
-	'Class for serial control of RC2800PX antenna rotators'
+	"""Class for serial control of RC2800PX antenna rotators"""
 
 	def __init__(self, port) :
 		self.rotor = serial.Serial(port,9600,timeout=0.1)
@@ -9,7 +9,7 @@ class Rotator :
 			self.rotor.close()
 		
 	def getAzimuth(self) :
-		"Returns the current Azimuth of the antenna"
+		"""Returns the current Azimuth of the antenna"""
 		self.rotor.flushInput();
 		# Request Azimuth mode
 		self.rotor.write("A\r")
@@ -21,7 +21,8 @@ class Rotator :
 		return float(response[startIndex:endIndex])
 	
 	def getElevation(self) :
-		"Returns the current Elevation of the antenna"
+		"""Returns the current Elevation of the antenna"""
+		self.rotor.flushInput();
 		# Request Elevation mode
 		self.rotor.write("E\r")
 		# Read response, example response: "E\r\nE=150.5 S=9 S\r\n"
@@ -32,7 +33,7 @@ class Rotator :
 		return float(response[startIndex:endIndex])
 		
 	def setPosition(self, reqAzimuth, reqElevation) :
-		"Commands rotor toward requested azimuth and elevation"
+		"""Commands rotor toward requested azimuth and elevation"""
 		# Ensure requested position is within limits
 		if reqAzimuth < 1: reqAzimuth = 1
 		if reqAzimuth > 359: reqAzimuth = 359
@@ -42,20 +43,31 @@ class Rotator :
 		# Issue Azimuth command
 		self.rotor.flushInput();
 		self.rotor.write("A" + str(reqAzimuth) + "\r")
-		print "Written: " + "A" + str(reqAzimuth) + "\r"
+		##print "Written: " + "A" + str(reqAzimuth) + "\r"
 		# readline() is critical to ensure correct timing
 		response = self.rotor.readline()
-		#print "Read: " + response
+		##print "Read: " + response
+		
 		# Issue Elevation command
 		self.rotor.flushInput();
 		self.rotor.write("E" + str(reqElevation) + "\r")
-		#print "Written: " + "E" + str(reqElevation) + "\r"
+		##print "Written: " + "E" + str(reqElevation) + "\r"
 		# readline() is critical to ensure correct timing
 		response = self.rotor.readline()
-		#print "Read: " + response
+		##print "Read: " + response
+		return
+		
+	def stop(self) :
+		"""Sends a stop command (S) to the rotator"""
+		print "Stopping...\r\n"
+		self.rotor.flushInput();
+		self.rotor.flushOutput();
+		self.rotor.write("S");
+		print self.rotor.readline()
 		return
 		
 	def printPosition(self) :
+		"""Reads and prints the current azimuth and elevation of the antenna"""
 		print("Azimuth: " + str(self.getAzimuth()) + " Elevation: " + str(self.getElevation()))
 		return
 # Test code
